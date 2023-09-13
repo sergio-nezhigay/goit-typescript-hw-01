4c69f1b7976475b44dbbd144a2965daf7a8b3e78
Sergio Nezhigay <abc1971h@gmail.com>
2023-09-13 08:51:27 +0300

    init

---
+++ dist/button.js
@@ -0,0 +1,10 @@
+"use strict";
+const button = document.querySelector("#buttonId");
+button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
+    let a = 5;
+    let b = 15;
+    b++;
+    console.log("click8", a);
+    console.log("click", b);
+});
+//# sourceMappingURL=button.js.map
